d028ebccf31ff7725aa515901129155d37257fab
Alima Grine <alima.grine@lyra-network.com>
2021-11-13 00:55:15 +0100

    Setup connection to mysql database.

---
+++ server/config/db.config.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+// Create mysql connection.
+const con = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: "kpmgtasks",
+});
+
+con.connect((err) => {
+  if (err) {
+    console.log(`Error connecting to database: ${err}`);
+    return;
+  }
+
+  console.log("Connection to database established");
+});
+
+module.exports = con;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Create express app.
 const app = express();
 dotenv.config();
 
+// Middleware.
+app.use(express.json({ limit: "30mb", extended: true })); // Parse request data content type application/json.
+app.use(express.urlencoded({ limit: "30mb", extended: true })); // Parse request data content type application/x-www-form-urlencoded.
+
+// Define root route.
+app.get("/", (req, res) => {
+  res.send("Hello to KPMG Task App REST API");
+});
+
+// Setup the server port.
 const PORT = process.env.PORT || 5000;
 
+// Listen to the port.
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
